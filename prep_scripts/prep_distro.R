ea0a80b0200dd6951b45627d36064b9eca0d9709
camille-s <camille.seaberry@gmail.com>
2020-01-23 17:04:41 -0500

    Finish draft 1 of distro scripts

--- prep_scripts/prep_distro.R
+++ prep_scripts/prep_distro.R
@@ -7,11 +7,6 @@ hdrs <- read_csv("_utils/indicator_headings.txt") %>%
   mutate(indicator = str_replace(indicator, "^(estimate|share)\\s(.+)$", "\\2_\\1")) %>%
   bind_rows(read_csv("_utils/headings_extra.txt"))
 
-
-# neighborhood profiles, individual files (?)
-nhoods <- list.files("to_distro", pattern = "neighborhood_2018.csv", full.names = TRUE) %>%
-  set_names(str_extract, "\\w+_neighborhood_2018.csv")
-
 # town, all one file
 town <- read_csv("output/acs_town_basic_profile_2018.csv") %>%
   select(level, name, group, estimate, share) %>%
@@ -26,22 +21,26 @@ town <- read_csv("output/acs_town_basic_profile_2018.csv") %>%
 
 write_csv(town, "to_distro/town_acs_basic_2018.csv")
 
-# all_files <- c(list(town_acs_basic_2018.csv = town), nhoods) 
-# 
-# all_files %>%
-#   imap(~dwapi::upload_data_frame(dataset = url, data_frame = .x, file_name = .y))
+
+# nhoods go on dw from gh; towns can just go up from this file
+# neighborhood profiles, individual files (?)
+nhood_req <- list.files("to_distro", pattern = "neighborhood_2018.csv") %>%
+  map(function(path) {
+    city <- path %>%
+      str_extract("\\w+_neighborhood_2018.csv") %>%
+      camiller::clean_titles(cap_all = TRUE)
+    desc <- paste("ACS basic indicators by neighborhood, 2018 5yr estimates,", city)
+    url <- paste0("https://github.com/CT-Data-Haven/2018acs/blob/master/to_distro/", path)
+    file_create_or_update_request(file_name = path, description = desc, labels = list("clean data"), url = url)
+  })
+
+walk(nhood_req, ~update_dataset(url, dataset_update_request(files = list(.))))
+
+dwapi::upload_file(url, "to_distro/town_acs_basic_2018.csv", "town_acs_basic_2018.csv")
+town_req <- file_create_or_update_request(file_name = "town_acs_basic_2018.csv", 
+                                          description = "ACS basic indicators by town and region, 2018 5yr estimates", 
+                                          labels = list("clean data"))
+update_dataset(url, dataset_update_request(files = list(town_req)))
 
 # add license: cc sharealike
 dwapi::update_dataset(url, dataset_update_request(license_string = "CC-BY-SA"))
-
-# add descriptions
-# nhood_desc <- names(nhoods) %>%
-#   str_extract("^\\w+(?=_acs)") %>%
-#   camiller::clean_titles(cap_all = TRUE) %>%
-#   paste("ACS basic indicators by neighborhood, 2018 5yr estimates,", .)
-# 
-# nhood_req <- map2(names(nhoods), nhood_desc, function(file, desc) {
-#   file_create_or_update_request(file_name = file, description = desc, labels = list("clean data"))
-# })
-# 
-# walk(nhood_req, ~update_dataset(url, dataset_update_request(files = list(.))))
